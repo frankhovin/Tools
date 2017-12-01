87e194841ed4cad060cf6d73339c95e3ced1d081
Frank Høvin <frankhovin@gmail.com>
2017-12-01 09:37:28 +0100

    Commit.

---
+++ ChromeMultipleLogins/NUnitTests/Tests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Forms;
+using NUnit.Extensions.Forms;   // NuGet: NUnitForms.Framework.
+using ChromeMultipleLogins;
+
+namespace NUnitTests {
+    //[TestFixture]
+    public class Tests {
+        [TestMethod]
+        public void TestMethod1 () {
+            Form mainform = new Form();
+            mainform.Show();
+
+            ButtonTester openChromeButton = new ButtonTester("buttonName");
+
+        }
+    }
+}
