c96395975d37c492ca5cd6c24de1b2d375842144
Frank Høvin <frankhovin@gmail.com>
2018-01-05 17:23:07 +0100

    Initial import.

---
+++ ChromeMullog/ChromeMullog/MainWindow.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace ChromeMullog {
+    partial class MainForm {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.websiteLabel = new System.Windows.Forms.Label();
+            this.websiteTextBox = new System.Windows.Forms.TextBox();
+            this.openChromeButton = new System.Windows.Forms.Button();
+            this.killButton = new System.Windows.Forms.Button();
+            this.addRowButton = new System.Windows.Forms.Button();
+            this.removeRowButton = new System.Windows.Forms.Button();
+            this.userPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.statusStrip = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // websiteLabel
+            // 
+            this.websiteLabel.AutoSize = true;
+            this.websiteLabel.Location = new System.Drawing.Point(13, 16);
+            this.websiteLabel.Name = "websiteLabel";
+            this.websiteLabel.Size = new System.Drawing.Size(59, 17);
+            this.websiteLabel.TabIndex = 0;
+            this.websiteLabel.Text = "Website";
+            // 
+            // websiteTextBox
+            // 
+            this.websiteTextBox.Location = new System.Drawing.Point(82, 16);
+            this.websiteTextBox.Name = "websiteTextBox";
+            this.websiteTextBox.Size = new System.Drawing.Size(294, 22);
+            this.websiteTextBox.TabIndex = 1;
+            // 
+            // openChromeButton
+            // 
+            this.openChromeButton.Location = new System.Drawing.Point(82, 67);
+            this.openChromeButton.Name = "openChromeButton";
+            this.openChromeButton.Size = new System.Drawing.Size(132, 27);
+            this.openChromeButton.TabIndex = 2;
+            this.openChromeButton.Text = "Open Chrome";
+            this.openChromeButton.UseVisualStyleBackColor = true;
+            // 
+            // killButton
+            // 
+            this.killButton.Location = new System.Drawing.Point(244, 67);
+            this.killButton.Name = "killButton";
+            this.killButton.Size = new System.Drawing.Size(133, 27);
+            this.killButton.TabIndex = 3;
+            this.killButton.Text = "Kill Chrome";
+            this.killButton.UseVisualStyleBackColor = true;
+            // 
+            // addRowButton
+            // 
+            this.addRowButton.Location = new System.Drawing.Point(244, 115);
+            this.addRowButton.Name = "addRowButton";
+            this.addRowButton.Size = new System.Drawing.Size(51, 23);
+            this.addRowButton.TabIndex = 4;
+            this.addRowButton.Text = "Add";
+            this.addRowButton.UseVisualStyleBackColor = true;
+            // 
+            // removeRowButton
+            // 
+            this.removeRowButton.Location = new System.Drawing.Point(301, 115);
+            this.removeRowButton.Name = "removeRowButton";
+            this.removeRowButton.Size = new System.Drawing.Size(75, 23);
+            this.removeRowButton.TabIndex = 5;
+            this.removeRowButton.Text = "Remove";
+            this.removeRowButton.UseVisualStyleBackColor = true;
+            // 
+            // userPanel
+            // 
+            this.userPanel.AutoSize = true;
+            this.userPanel.ColumnCount = 3;
+            this.userPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 60F));
+            this.userPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.userPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.userPanel.Location = new System.Drawing.Point(9, 153);
+            this.userPanel.Margin = new System.Windows.Forms.Padding(0, 0, 0, 35);
+            this.userPanel.Name = "userPanel";
+            this.userPanel.RowCount = 3;
+            this.userPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.userPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.userPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.userPanel.Size = new System.Drawing.Size(392, 103);
+            this.userPanel.TabIndex = 6;
+            // 
+            // statusStrip
+            // 
+            this.statusStrip.GripStyle = System.Windows.Forms.ToolStripGripStyle.Visible;
+            this.statusStrip.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel});
+            this.statusStrip.Location = new System.Drawing.Point(0, 267);
+            this.statusStrip.Name = "statusStrip";
+            this.statusStrip.Size = new System.Drawing.Size(406, 25);
+            this.statusStrip.TabIndex = 7;
+            this.statusStrip.Text = "statusStrip";
+            // 
+            // toolStripStatusLabel
+            // 
+            this.toolStripStatusLabel.Name = "toolStripStatusLabel";
+            this.toolStripStatusLabel.Size = new System.Drawing.Size(151, 20);
+            this.toolStripStatusLabel.Text = "toolStripStatusLabel1";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(406, 292);
+            this.Controls.Add(this.statusStrip);
+            this.Controls.Add(this.userPanel);
+            this.Controls.Add(this.removeRowButton);
+            this.Controls.Add(this.addRowButton);
+            this.Controls.Add(this.killButton);
+            this.Controls.Add(this.openChromeButton);
+            this.Controls.Add(this.websiteTextBox);
+            this.Controls.Add(this.websiteLabel);
+            this.Name = "MainForm";
+            this.Text = "ChromeMullog";
+            this.statusStrip.ResumeLayout(false);
+            this.statusStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label websiteLabel;
+        private System.Windows.Forms.TextBox websiteTextBox;
+        private System.Windows.Forms.Button openChromeButton;
+        private System.Windows.Forms.Button killButton;
+        private System.Windows.Forms.Button addRowButton;
+        private System.Windows.Forms.Button removeRowButton;
+        private System.Windows.Forms.TableLayoutPanel userPanel;
+        private System.Windows.Forms.StatusStrip statusStrip;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel;
+    }
+}
+
---
+++ ChromeMullog/ChromeMullog/MainWindow.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Windows.Forms;
+//using Selenium;
+//using ChromeMultipleLogins.Properties;
+//using WindowsOperations.Authentication;
+using System.Diagnostics;
+using System.Collections;
+
+namespace ChromeMullog {
+    public partial class MainForm : Form {
+        public MainForm() {
+            InitializeComponent();
+
+            if (Properties.Settings.Default.rowcount == 0) {
+                Properties.Settings.Default.rowcount = 3;
+            }
+            for (int i = 0; i < Properties.Settings.Default.rowcount; i++) {
+                userPanel.Controls.Add(new Label() { Text = "Login" });
+                userPanel.Controls.Add(new TextBox());
+                userPanel.Controls.Add(new TextBox());
+            }
+
+            //toolStripStatusLabel.Text = "FAEN!";
+            toolStripStatusLabel.Text = "Stored row count: " + Properties.Settings.Default.rowcount.ToString();
+            // DETTE VISER INGENTING, FORDI DET IKKE BLIR LAGRET NOE I users NÅR SaveEntries() KJØRER!
+            if (Properties.Settings.Default.users != null)
+                toolStripStatusLabel.Text += "Stored u/p count: " + Properties.Settings.Default.users.Count.ToString();
+
+            PopulateForm();
+        }
+
+        /**
+         * Populate form with default values.
+         */
+        private void PopulateForm() {
+            // Load the url value from the application.settings file:
+            websiteTextBox.Text = Properties.Settings.Default.lasturl;
+
+            // Load the username and password values from the application.settings file:
+            int y = 0;
+            try {
+                for (int i = 0; i < Properties.Settings.Default.users.Count / 2; i++) {
+                    userPanel.GetControlFromPosition(1, i).Text = Properties.Settings.Default.users[y];
+                    y++;
+                    userPanel.GetControlFromPosition(2, i).Text = Properties.Settings.Default.users[y];
+                    y++;
+                }
+            } catch (NullReferenceException) { }
+        }
+
+
+
+
+
+        private void killChromeProcesses() {
+
+        }
+
+        /**
+         * Handler for performing actions when the application quits.
+         */
+        protected override void OnFormClosing(FormClosingEventArgs e) {
+            base.OnFormClosing(e);
+            if (PreClosingConfirmation() == System.Windows.Forms.DialogResult.Yes) {
+                killChromeProcesses();
+                SaveEntries();
+                Dispose(true);
+                Application.Exit();
+            }
+            else {
+                e.Cancel = true;
+            }
+        }
+
+        /**
+         * Quit confirmation dialog.
+         */
+        private DialogResult PreClosingConfirmation() {
+            DialogResult res = System.Windows.Forms.MessageBox.Show(
+                "Do you want to quit?",
+                "Quit Chrome Multiple Logins", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            return res;
+        }
+
+        /**
+        * Save the data in the text fields to the application.settings file.
+        */
+        private void SaveEntries() {
+            // Add the contents of the url text box to the settings file (replacing the existing value):
+            Properties.Settings.Default.lasturl = websiteTextBox.Text;
+
+            // Clear the existing collection of usernames/passwords:
+            // If the users StringCollection exists, just clear it of all contents.
+            // If it doesn't exist, create it.
+            if (Properties.Settings.Default.users != null) {
+                Properties.Settings.Default.users.Clear();
+            } else {
+                Properties.Settings.Default.users = new System.Collections.Specialized.StringCollection();
+            }
+
+            // For troubleshooting: Show a message box with the state of the users StringCollection (null or not):
+            /*String usersstatus = "";
+            if (Properties.Settings.Default.users == null)
+                usersstatus = "NULL";
+            if (Properties.Settings.Default.users != null)
+                usersstatus = "IKKE NULL";
+            MessageBox.Show(usersstatus);*/
+
+            // Save the contents of the username/password rows (including blank entries):
+            // SER UT TIL AT IKKE Properties.Settings.Default.users ER INITIALISERT? MULIG DERFOR DET FEILER MED NullReferenceException HER:
+            // ER DET EN IDÉ Å GI Properties.Settings.Default.users EN DEFAULT VERDI? I SÅFALL, HVORDAN? PÅ SAMME MÅTE SOM ROWCOUNT?
+            // DET FUNGERER IKKE Å LAGRE NOE I users!
+            //for (int i = 0; i < userPanel.RowCount; i++) {
+            for (int i = 0; i < Properties.Settings.Default.rowcount; i++) {
+                if (Properties.Settings.Default.users != null) {
+                    Properties.Settings.Default.users.Add(userPanel.GetControlFromPosition(1, i).Text);
+                    Properties.Settings.Default.users.Add(userPanel.GetControlFromPosition(2, i).Text);
+                }
+            }
+
+
+
+            // Save the settings file:
+            Properties.Settings.Default.Save();
+        }
+    }
+}
