6240c80b562c72af47cfb96e2f3b40df726541e2
Frank Høvin <frankhovin@gmail.com>
2018-02-21 09:38:01 +0100

    Commit

--- ChromeMultipleInstances/ChromeMultipleInstances/MainForm.Designer.cs
+++ ChromeMultipleInstances/ChromeMultipleInstances/MainForm.Designer.cs
@@ -25,10 +25,15 @@
         private void InitializeComponent() {
             this.funcSelector = new MetroFramework.Controls.MetroComboBox();
             this.ssMultipleLoginsPanel = new MetroFramework.Controls.MetroPanel();
+            this.passwordTextbox = new MetroFramework.Controls.MetroTextBox();
+            this.usernameTextbox = new MetroFramework.Controls.MetroTextBox();
+            this.passwordLabel = new MetroFramework.Controls.MetroLabel();
+            this.usernameLabel = new MetroFramework.Controls.MetroLabel();
+            this.ssMultipleLoginsPanel.SuspendLayout();
             this.SuspendLayout();
-            // 
+            //
             // funcSelector
-            // 
+            //
             this.funcSelector.FormattingEnabled = true;
             this.funcSelector.ItemHeight = 24;
             this.funcSelector.Items.AddRange(new object[] {
@@ -38,10 +43,14 @@
             this.funcSelector.Size = new System.Drawing.Size(490, 30);
             this.funcSelector.TabIndex = 0;
             this.funcSelector.SelectedIndexChanged += new System.EventHandler(this.funcSelector_SelectedIndexChanged);
-            // 
+            //
             // ssMultipleLoginsPanel
-            // 
+            //
             this.ssMultipleLoginsPanel.BorderStyle = MetroFramework.Drawing.MetroBorderStyle.FixedSingle;
+            this.ssMultipleLoginsPanel.Controls.Add(this.passwordTextbox);
+            this.ssMultipleLoginsPanel.Controls.Add(this.usernameTextbox);
+            this.ssMultipleLoginsPanel.Controls.Add(this.passwordLabel);
+            this.ssMultipleLoginsPanel.Controls.Add(this.usernameLabel);
             this.ssMultipleLoginsPanel.HorizontalScrollbarBarColor = true;
             this.ssMultipleLoginsPanel.HorizontalScrollbarHighlightOnWheel = false;
             this.ssMultipleLoginsPanel.HorizontalScrollbarSize = 10;
@@ -52,9 +61,41 @@
             this.ssMultipleLoginsPanel.VerticalScrollbarBarColor = true;
             this.ssMultipleLoginsPanel.VerticalScrollbarHighlightOnWheel = false;
             this.ssMultipleLoginsPanel.VerticalScrollbarSize = 10;
-            // 
+            //
+            // usernameTextbox
+            //
+            this.usernameTextbox.Location = new System.Drawing.Point(90, 90);
+            this.usernameTextbox.Name = "usernameTextbox";
+            this.usernameTextbox.Size = new System.Drawing.Size(190, 25);
+            this.usernameTextbox.TabIndex = 4;
+            //
+            // passwordTextbox
+            //
+            this.passwordTextbox.Location = new System.Drawing.Point(290, 90);
+            this.passwordTextbox.Name = "passwordTextbox";
+            this.passwordTextbox.Size = new System.Drawing.Size(190, 25);
+            this.passwordTextbox.TabIndex = 5;
+            //
+            // usernameLabel
+            //
+            this.usernameLabel.AutoSize = true;
+            this.usernameLabel.Location = new System.Drawing.Point(90, 60);
+            this.usernameLabel.Name = "usernameLabel";
+            this.usernameLabel.Size = new System.Drawing.Size(73, 20);
+            this.usernameLabel.TabIndex = 2;
+            this.usernameLabel.Text = "Username";
+            //
+            // passwordLabel
+            //
+            this.passwordLabel.AutoSize = true;
+            this.passwordLabel.Location = new System.Drawing.Point(290, 60);
+            this.passwordLabel.Name = "passwordLabel";
+            this.passwordLabel.Size = new System.Drawing.Size(66, 20);
+            this.passwordLabel.TabIndex = 3;
+            this.passwordLabel.Text = "Password";
+            //
             // MainForm
-            // 
+            //
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
@@ -63,6 +104,8 @@
             this.Controls.Add(this.funcSelector);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "MainForm";
+            this.ssMultipleLoginsPanel.ResumeLayout(false);
+            this.ssMultipleLoginsPanel.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -73,6 +116,10 @@
         private MetroFramework.Controls.MetroPanel ssMultipleLoginsPanel;
         private MetroFramework.Controls.MetroTextBox address1Textbox;
         private MetroFramework.Controls.MetroLabel adr1Label;
+        private MetroFramework.Controls.MetroLabel usernameLabel;
+        private MetroFramework.Controls.MetroLabel passwordLabel;
+        private MetroFramework.Controls.MetroTextBox passwordTextbox;
+        private MetroFramework.Controls.MetroTextBox usernameTextbox;
     }
 }
 
--- ChromeMultipleInstances/ChromeMultipleInstances/MainForm.cs
+++ ChromeMultipleInstances/ChromeMultipleInstances/MainForm.cs
@@ -34,7 +34,7 @@ namespace ChromeMultipleInstances {
             MetroFramework.Controls.MetroLabel addr1Label = new MetroFramework.Controls.MetroLabel();
             ssMultipleLoginsPanel.Controls.Add(addr1Label);
             addr1Label.AutoSize = true;
-            addr1Label.Location = new System.Drawing.Point(4, 10);
+            addr1Label.Location = new System.Drawing.Point(4, 12);
             addr1Label.Name = "adr1Label";
             addr1Label.Size = new System.Drawing.Size(55, 20);
             addr1Label.TabIndex = 2;
@@ -43,11 +43,44 @@ namespace ChromeMultipleInstances {
             MetroFramework.Controls.MetroTextBox addr1Textbox = new MetroFramework.Controls.MetroTextBox();
             ssMultipleLoginsPanel.Controls.Add(addr1Textbox);
             addr1Textbox.Location = new System.Drawing.Point(70, 10);
-            //addr1Textbox.Name = "address1Textbox";
             addr1Textbox.Size = new System.Drawing.Size(290, 23);
             addr1Textbox.Style = MetroFramework.MetroColorStyle.White;
             addr1Textbox.TabIndex = 4;
             addr1Textbox.Text = "Website address";
+
+            //
+            // usernameLabel
+            //
+            this.usernameLabel.AutoSize = true;
+            this.usernameLabel.Location = new System.Drawing.Point(90, 60);
+            this.usernameLabel.Name = "usernameLabel";
+            this.usernameLabel.Size = new System.Drawing.Size(73, 20);
+            this.usernameLabel.TabIndex = 2;
+            this.usernameLabel.Text = "Username";
+            //
+            // passwordLabel
+            //
+            this.passwordLabel.AutoSize = true;
+            this.passwordLabel.Location = new System.Drawing.Point(290, 60);
+            this.passwordLabel.Name = "passwordLabel";
+            this.passwordLabel.Size = new System.Drawing.Size(66, 20);
+            this.passwordLabel.TabIndex = 3;
+            this.passwordLabel.Text = "Password";
+            //
+            // usernameTextbox
+            //
+            this.usernameTextbox.Location = new System.Drawing.Point(90, 90);
+            this.usernameTextbox.Name = "usernameTextbox";
+            this.usernameTextbox.Size = new System.Drawing.Size(190, 25);
+            this.usernameTextbox.TabIndex = 4;
+            //
+            // passwordTextbox
+            //
+            this.passwordTextbox.Location = new System.Drawing.Point(290, 90);
+            this.passwordTextbox.Name = "passwordTextbox";
+            this.passwordTextbox.Size = new System.Drawing.Size(190, 25);
+            this.passwordTextbox.TabIndex = 5;
+
 
 
         }
